b7cd8b58bb6e77bd97f695135bec078a245fb884
wenhao <qiaowenhao@gmail.com>
2015-10-24 19:52:30 +0800

    添加目录和文章的页面

--- server.js
+++ server.js
@@ -2,31 +2,50 @@ var express = require('express');
 
 var app = express();
 
-app.set('port',process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/category', function(request, response){
-    response.sendfile('./public/category.html');
+var options = {
+    root: __dirname + '/public/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+};
+
+app.get('/index', function (request, response) {
+    response.sendFile('index.html', options, function () {
+        console.log('visited index');
+    });
 });
 
-app.get('/article',function(request,response){
-    response.sendfile('./public/article.html');
+app.get('/category', function (request, response) {
+    response.sendFile('category.html', options, function () {
+        console.log('visited category');
+    });
 });
 
-app.use(function(err, request, response, next){
+app.get('/article', function (request, response) {
+    response.sendFile('article.html', options, function () {
+        console.log('visited index');
+    });
+});
+
+app.use(function (err, request, response, next) {
     console.error(err.stack);
     response.type('text/plain');
     response.status(500);
     response.send('500 - server internal error');
 });
 
-app.use(function(request, response){
+app.use(function (request, response) {
     response.type('text/plain');
     response.status(404);
     response.send('404 - not found');
 });
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
     console.log('Express started on http://localhost:' + app.get('port'));
 });
