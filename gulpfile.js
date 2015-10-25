c1a00184a7fa8b00e08eb021f791f004aa405332
wenhao <qiaowenhao@gmail.com>
2015-10-25 21:39:31 +0800

    添加gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var browserSync = require('browser-sync');
+
+gulp.task('debug-cover', function () {
+    return browserify({entries: 'public/javascripts/cover.js', debug: true})
+        .transform(babelify)
+        .bundle()
+        .pipe(source('cover.bundle.js'))
+        .pipe(gulp.dest('public/javascripts'));
+});
+
+gulp.task('debug-category', function () {
+    return browserify({entries: 'public/javascripts/category.js', debug: true})
+        .transform(babelify)
+        .bundle()
+        .pipe(source('category.bundle.js'))
+        .pipe(gulp.dest('public/javascripts'));
+});
+
+gulp.task('debug', ['debug-cover','debug-category']);
+
+//gulp.task('browserSync', function(){
+//
+//});
+
+//gulp.task('watch', ['debug'], function () {
+//    gulp.watch('*.js', ['debug']);
+//});
+
+//gulp.task('default', ['watch']);
