bc336b642f3dcc61498c5d90763db3c8100bf21b
wenhao <qiaowenhao@gmail.com>
2015-11-07 21:56:40 +0800

    重构部分代码

--- public/javascripts/category.js
+++ public/javascripts/category.js
@@ -1,4 +1,3 @@
-var React = require('react');
 var ReactDOM = require('react-dom');
 var Category = require('./components/category.react');
 
--- public/javascripts/cover.js
+++ public/javascripts/cover.js
@@ -1,4 +1,3 @@
-var React = require('react');
 var ReactDOM = require('react-dom');
 var Cover = require('./components/cover.react');
 
