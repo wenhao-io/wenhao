8b9806ed353de3e544d5ac4bc1ce4e44b8865101
wenhao <qiaowenhao@gmail.com>
2016-02-14 15:06:58 +0800

    ...

--- public/javascripts/components/navigation.react.js
+++ public/javascripts/components/navigation.react.js
@@ -1,7 +1,6 @@
 var React = require('react');
 
-var Navigation = React.createClass(
-    {
+var Navigation = React.createClass({
         render: function () {
             return <nav className="navbar navbar-inverse navbar-fixed-top">
                 <div className="container">
@@ -10,9 +9,9 @@ var Navigation = React.createClass(
                                 data-target="#navbar"
                                 aria-expanded="false" aria-controls="navbar">
                             <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
+                            <span className="icon-bar"/>
+                            <span className="icon-bar"/>
+                            <span className="icon-bar"/>
                         </button>
                         <a className="navbar-brand" href="/index">WENHAO.IO</a>
                     </div>
@@ -20,6 +19,17 @@ var Navigation = React.createClass(
                         <ul className="nav navbar-nav">
                             <li className="active"><a href="/category">目录</a></li>
                         </ul>
+                        <form className="navbar-form navbar-right">
+                            {
+                                //<div className="form-group">
+                                //    <input type="text" placeholder="Email" className="form-control"/>
+                                //</div>
+                                //<div className="form-group">
+                                //<input type="password" placeholder="Password" className="form-control"/>
+                                //</div>
+                            }
+                            <button className="btn btn-success">登录 | 注册</button>
+                        </form>
                     </div>
                 </div>
             </nav>;
